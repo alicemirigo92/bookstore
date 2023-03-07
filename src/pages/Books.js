090663c1a731730fe74bb7277a3c631eaba4b6bb
alicemirigo92 <alicemirigo92@gmai.com>
2023-03-07 17:50:07 +0300

    Add redux

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import BookList from '../components/BookList';
-import AddBook from '../components/AddBook';
+// import React from 'react';
+// import BookList from '../components/BookList';
+// import AddBook from '../components/AddBook';
 
-function Books() {
-  return (
-    <div>
-      <BookList />
-      <AddBook />
-    </div>
-  );
-}
+// function Books() {
+//   return (
+//     <div>
+//       <BookList />
+//       <AddBook />
+//     </div>
+//   );
+// }
 
-export default Books;
+// export default Books;
